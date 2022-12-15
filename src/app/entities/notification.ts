3ac17a09f318160745f48164c33da9525d2defe3
phdevbr <pedroheringer2020@gmail.com>
2022-12-15 13:47:28 -0300

    removing jest from json

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,51 +1,53 @@
+import { Content } from "./content";
+
 export interface NotificationProps {
-  recipientId: string;
-  content: string;
-  category: string;
-  readAt?: Date | null;
-  createdAt: Date;
+    recipientId: string;
+    content: Content;
+    category: string;
+    readAt?: Date | null;
+    createdAt: Date;
 }
 
 export class Notification {
-  private props: NotificationProps;
+    private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
-  }
+    constructor(props: NotificationProps) {
+        this.props = props;
+    }
 
-  public set recipientId(recipientId: string) {
-    this.props.recipientId = recipientId;
-  }
+    public set recipientId(recipientId: string) {
+        this.props.recipientId = recipientId;
+    }
 
-  public get recipientId(): string {
-    return this.props.recipientId;
-  }
+    public get recipientId(): string {
+        return this.props.recipientId;
+    }
 
-  public set content(content: string) {
-    this.props.content = content;
-  }
+    public set content(content: Content) {
+        this.props.content = content;
+    }
 
-  public get content(): string {
-    return this.props.content;
-  }
+    public get content(): Content {
+        return this.props.content;
+    }
 
-  public set category(category: string) {
-    this.props.category = category;
-  }
+    public set category(category: string) {
+        this.props.category = category;
+    }
 
-  public get category(): string {
-    return this.props.category;
-  }
+    public get category(): string {
+        return this.props.category;
+    }
 
-  public set readAt(readAt: Date | null | undefined) {
-    this.props.readAt = readAt;
-  }
+    public set readAt(readAt: Date | null | undefined) {
+        this.props.readAt = readAt;
+    }
 
-  public get readAt(): Date | null | undefined {
-    return this.props.readAt;
-  }
+    public get readAt(): Date | null | undefined {
+        return this.props.readAt;
+    }
 
-  public get createdAt(): Date {
-    return this.props.createdAt;
-  }
+    public get createdAt(): Date {
+        return this.props.createdAt;
+    }
 }
