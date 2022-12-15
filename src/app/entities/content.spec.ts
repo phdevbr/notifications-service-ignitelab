43b36d40c5925daf6a91d9ed4166479164c36b5f
phdevbr <pedroheringer2020@gmail.com>
2022-12-15 14:26:58 -0300

    added tests

---
+++ src/app/entities/content.spec.ts
@@ -0,0 +1,11 @@
+import { Content } from "./content";
+
+test("it should be able to create a notification content", () => {
+    const content = new Content("Você recebeu uma solicitação de amizade!");
+
+    expect(content).toBeTruthy();
+});
+
+test("it should not be able to create a notification content with less than 5 characters", () => {
+    expect(() => new Content("abc")).toThrow();
+});
